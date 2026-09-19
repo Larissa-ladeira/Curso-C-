4ce4c4529ee0aa5641bddb77d3e11fcfa019aa20
Larissa Cristina Ladeira <larissaladeira42@gmail.com>
2026-09-19 12:57:27 -0300

    passagem por valor e por referencia

--- Aulas/Aula10/obj/Debug/net10.0/Aula10.AssemblyInfo.cs
+++ Aulas/Aula10/obj/Debug/net10.0/Aula10.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula10")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5be8cec82de79a72e27f7e1c690bca05b257515d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fbe7865beaa1985cd791d6e5e0c4cf9f03da2b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula10")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula10")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Aulas/Aula25/Aula25.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Aula25{
+    static void Main(){
+        int num=10;
+        dobrar1(ref num);
+        Console.WriteLine(num);
+        
+    }
+    static void dobrar1(ref int valor){
+        valor*=2; 
+    }
+
+    static void dobrar2(int valor){
+        valor*=2; 
+    }
+}
